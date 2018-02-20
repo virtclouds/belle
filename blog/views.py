be4722f7412aa2c974c9a889193c11f397b9141d
Zhaokun Fu <zhaokun.fu@gmail.com>
2018-02-20 13:39:36 +0800

    custom template tags
    1. recent posts
    2. archives
    3. categorys

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from .models import Post
+from .models import Post, Category
 import markdown
 
 
@@ -22,3 +22,15 @@ def detail(request, pk):
                                      'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
