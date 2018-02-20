79c93740bd4f41c463730bc9d234adb01d603acf
Zhaokun Fu <zhaokun.fu@gmail.com>
2018-02-20 14:23:38 +0800

    IndexView

--- blog/views.py
+++ blog/views.py
@@ -6,14 +6,21 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import Post, Category
 import markdown
+from django.views.generic import ListView
 
 
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+
+    #阅读量+1
+    post.increase_views()
+
     # 记得在顶部引入 markdown 模块
     post.body = markdown.markdown(post.body,
                                   extensions=[
